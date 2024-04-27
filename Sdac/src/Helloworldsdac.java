ed63561ebe3310fd2acd6cdeb6eb8190b43a6d0b
sdac-5 <sdac-5@sdac-5-PC>
2024-04-27 14:48:30 +0530

    new print

--- Sdac/src/Helloworldsdac.java
+++ Sdac/src/Helloworldsdac.java
@@ -2,6 +2,8 @@
 public class Helloworldsdac {
 	public static void main(String[] args){
 		System.out.println("harikesh sharma");
+		System.out.println("hello2now");
+		System.out.println("hello2now");
 	}
 
 }
